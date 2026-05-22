732942f55dddc241bab6dd38edee13dd3f8e63f4
Eb0b0l0 <89797412+ebobo54@users.noreply.github.com>
2026-05-22 12:28:37 +0500

    1

--- lab11/main.py
+++ lab11/main.py
@@ -6,6 +6,7 @@ from kivy.uix.label import Label
 from kivy.clock import Clock
 import random as r
 
+
 class Mark(Button):
     size = (20, 20)
     col = ((0, 0, 0, 0), (0.5, 0.5, 0.5, 1), (0, 0, 0, 1))
@@ -14,6 +15,7 @@ class Mark(Button):
     def a(self):
         self.background_color = self.col[self.val]
 
+
 class Pin(Button):
     alf = 1
     col = ((1, 0, 0, alf), (0, 1, 0, alf), (0, 0, 1, alf),
@@ -21,19 +23,29 @@ class Pin(Button):
     val = -1
     size = (50, 50)
 
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.update_color()
+
     def on_press(self):
         if self.val + 1 < len(self.col):
             self.val += 1
         else:
             self.val = 0
-        self.background_color = self.col[self.val]
+        self.update_color()
+
+    def update_color(self):
+        if 0 <= self.val < len(self.col):
+            self.background_color = self.col[self.val]
+        else:
+            self.background_color = (0, 0, 0, 0)
+
 
 class Board(Widget):
     def Start(self):
         self.pins = []
         for a in range(4):
             b = Pin()
-            b.on_press()
             b.pos = (self.pos[0] + 70 + a * 60, self.pos[1] + 10)
             self.add_widget(b)
             self.pins.append(b)
@@ -49,38 +61,49 @@ class Board(Widget):
             a.a()
             self.add_widget(a)
 
+
 Builder.load_string("""
 <Board>:
+    size_hint: None, None
+    size: 310, 70
     canvas:
         Color:
             rgba: 1, 1, 1, 1
         Rectangle:
             pos: self.pos
-            size: 310, 70
+            size: self.size
 """)
 
+
 class MasterMind(Widget):
-    h = None
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.h = None
+        self.board_height = 70
+        self.margin = 10
 
     def Test(self, inp, coder):
         out = []
         code = list(coder)
         inp_copy = list(inp)
-        for a in range(4):
-            if code[a] == inp_copy[a]:
+
+        for i in range(4):
+            if inp_copy[i] == code[i]:
                 out.append(2)
-                code[a] = "a"
-                inp_copy[a] = 80.5
-        for a in range(4):
-            if inp_copy[a] in code:
+                code[i] = -1
+                inp_copy[i] = -2
+
+        for i in range(4):
+            if inp_copy[i] >= 0 and inp_copy[i] in code:
                 out.append(1)
-        for _ in range(4 - len(out)):
-            out.append(0)
+                code[code.index(inp_copy[i])] = -1
+
+        out += [0] * (4 - len(out))
         return out
 
     def Check(self, button):
         inp = [pin.val for pin in button.parent.pins]
-        if inp != self.code:
+        if inp != list(self.code):
             button.parent.SumUp(self.Test(inp, self.code))
             button.parent.remove_widget(button)
             if self.attempts < 5:
@@ -92,21 +115,24 @@ class MasterMind(Widget):
             self.EndGame(True)
 
     def SpawnBoard(self, Npos):
-        a = Board()
-        a.pos = (0, self.h - 80 * Npos)
-        a.Start()
-        submit_btn = Button(text="Submit", size=(50, 50),
-                            pos=(a.pos[0] + 10, a.pos[1] + 10),
-                            on_press=self.Check)
-        a.add_widget(submit_btn)
-        self.add_widget(a)
-        self.b.append(a)
+        board = Board()
+        y_top = self.h - Npos * (self.board_height + self.margin)
+        board.pos = (0, y_top - self.board_height)
+        board.Start()
+        submit = Button(
+            text="Submit",
+            size=(50, 30),
+            pos=(board.pos[0] + 5, board.pos[1] + 5),
+            on_press=self.Check
+        )
+        board.add_widget(submit)
+        self.add_widget(board)
+        self.b.append(board)
 
     def Setup(self, *args):
         self.attempts = 0
         self.b = []
-        if self.h is None:
-            self.h = self.height - 10
+        self.h = self.height - self.margin
         self.code = tuple(r.randint(0, 5) for _ in range(4))
         self.SpawnBoard(0)
 
@@ -119,18 +145,29 @@ class MasterMind(Widget):
 
     def EndGame(self, condition):
         win_text = "You Won" if condition else "You Lost"
-        self.WinLabel = Label(text=win_text, pos=(500, 500), font_size=50)
+        self.WinLabel = Label(
+            text=win_text,
+            size_hint=(None, None),
+            size=(200, 50),
+            pos=(self.center_x - 100, self.center_y + 25),
+            font_size=40
+        )
         self.add_widget(self.WinLabel)
-        restart_btn = Button(text="Restart", pos=(450, 470), size=(200, 50),
-                             on_press=self.Restart)
+        restart_btn = Button(
+            text="Restart",
+            size=(150, 50),
+            pos=(self.center_x - 75, self.center_y - 50),
+            on_press=self.Restart
+        )
         self.add_widget(restart_btn)
+
 
 class MasterMindApp(App):
     def build(self):
         game = MasterMind()
-
         Clock.schedule_once(game.Setup, 0)
         return game
+
 
 if __name__ == '__main__':
     MasterMindApp().run()
